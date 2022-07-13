10fafd1151119217b22afd73a3b1384386e6086a
__Rals <winhave@naver.com>
2022-07-13 10:09:37 +0900

    📑 :: 푸터 컴포넌트 추가

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const Footer = () => {
+  return (
+    <Wrapper>
+      <strong>ⓒ 2022 No Named All rights reserved.</strong>
+      <span>Front-End : __Rals</span>
+      <span>Back-End : chlgml</span>
+      <span>Embeded : choiyj0812</span>
+      <span>(Dummy data, These can be changed.)</span>
+    </Wrapper>
+  );
+};
+
+export default Footer;
+
+const Wrapper = styled.div`
+  background-color: #f6f6f6;
+  position: relative;
+
+  width: 100%;
+  height: 8rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  font-family: "B612";
+  font-size: 1rem;
+
+  span {
+    font-size: 0.75rem;
+    :first-of-type {
+      margin-top: 0.5rem;
+    }
+  }
+`;
