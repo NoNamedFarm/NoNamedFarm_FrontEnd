097c43b4aa160a510d7ca253fc0ebb1dce983f5b
__Rals <winhave@naver.com>
2022-07-14 20:16:41 +0900

    📑 :: 이미지 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,4 @@
 export { default as banner } from "./banner.png";
 export { default as farmer } from "./farmer.png";
 export { default as icon } from "./icon.png";
+export { default as logo } from "./logo.png";
